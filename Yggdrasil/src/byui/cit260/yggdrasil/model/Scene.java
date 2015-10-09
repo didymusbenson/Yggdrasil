d1a819f6bf4c5e167273026ec98266396704963c
Tanvar2 <Tanvar2@gmail.com>
2015-10-09 09:44:10 -0600

    10/9/2015 - This update gives only some notes.

--- Yggdrasil/src/byui/cit260/yggdrasil/model/Scene.java
+++ Yggdrasil/src/byui/cit260/yggdrasil/model/Scene.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Program type: Class
+ * Creation: 7/10/2015 at 9:59
+ * Description: This class gives the program the ability give different 
+ *              elements of the game at different areas of the map.
  */
 package byui.cit260.yggdrasil.model;
 
