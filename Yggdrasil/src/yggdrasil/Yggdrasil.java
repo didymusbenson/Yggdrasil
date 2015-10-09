b93cabda69f2d61d974ea43aa5673ed3d8cba175
Tanvar2 <Tanvar2@gmail.com>
2015-10-09 10:20:26 -0600

    10/9/2015 - This update gives the program map for the main function. Other comments may also have been added.

--- Yggdrasil/src/yggdrasil/Yggdrasil.java
+++ Yggdrasil/src/yggdrasil/Yggdrasil.java
@@ -14,9 +14,20 @@
  *   1 - package
  *   2 - import
  *   3 - MainCharacter class call
+ *   4 - Yggdrasil class call
+ *      4-01 - main initialization
+ *      4-02 - MAIN CHARACTER
+ *      4-03 - ACTOR
+ *      4-04 - DUNGON
+ *      4-05 - ENEMY
+ *      4-06 - LOCATION
+ *      4-07 - PLAYER
+ *      4-08 - SCENE
  */
+// 1 - this tells the program what game is to be played.
 package yggdrasil;
 
+// 2 - This tells the program what it will need to play the game.
 import byui.cit260.yggdrasil.model.Actor;
 import byui.cit260.yggdrasil.model.Dungon;
 import byui.cit260.yggdrasil.model.Enemy;
@@ -27,15 +38,17 @@ import byui.cit260.yggdrasil.model.Scene;
 
 /**
  *
- * @author tsunami
+ * @author Brock Hodgson & Didymus Benson
  */
+// 4 - this calls the starting point so the game can run.
 public class Yggdrasil {
 
     /**
      * @param args the command line arguments
      */
+    // 4-01 - this is the main that controls the rest of the game.
     public static void main(String[] args) {
-        //MAIN CHARACTER
+        // 4-02 - MAIN CHARACTER sets, and then a command to display those sets.
         MainCharacter alice = new MainCharacter();
         alice.setMainCharacterLuck(3);
         alice.setMainCharacterSpeed(5);
@@ -44,7 +57,7 @@ public class Yggdrasil {
         alice.setMainCharacterCurrentLevel(2);
         System.out.println(alice.toString());
 
-        //ACTOR
+        // 4-03 - ACTOR
         Actor sprite = new Actor();
         sprite.setActorMaxHp(10);
         sprite.setActorTempHp(5);
@@ -53,20 +66,20 @@ public class Yggdrasil {
         sprite.setActorWeapon(5);
         System.out.println(sprite.toString());
 
-        //DUNGON
+        // 4-04 - DUNGON
         Dungon lair = new Dungon();
         lair.setDungonGoldAvalible(35);
         lair.setDungonNumberOfFights(12);
         System.out.println(lair.toString());
 
-        //ENEMY 
+        // 4-05 - ENEMY 
         Enemy bob = new Enemy();
         bob.setEnemyEscapeChance(80);
         bob.setEnemyGoldReward(14);
         bob.setEnemyXpReward(34);
         System.out.println(bob.toString());
 
-        // LOCATION
+        // 4-06 - LOCATION
         Location terra = new Location();
         terra.setLocationCoordinates(8.4);
         terra.setLocationEncounterChance(40);
@@ -74,12 +87,12 @@ public class Yggdrasil {
         terra.setLocationHostility(1);
         System.out.println(terra.toString());
 
-        //PLAYER
+        // 4-07 PLAYER
         Player hater = new Player();
         hater.setPlayerName("Tom");
         System.out.println(hater.toString());
 
-        //SCENE
+        // 4-08 - SCENE
         Scene scene = new Scene();
         scene.setSceneDescription("fun");
         scene.setSceneHasBarrier(true);
