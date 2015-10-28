da34e2e1f347c24ec7fe04e34344b7a3bcfb29c2
Didymus <Didymus.Benson@gmail.com>
2015-10-28 09:37:57 -0600

    10-28-2015 getName function implemented to SPV -ADB

--- Yggdrasil/test/byui/cit260/yggdrasil/view/StartProgramViewTest.java
+++ Yggdrasil/test/byui/cit260/yggdrasil/view/StartProgramViewTest.java
@@ -24,7 +24,7 @@ public class StartProgramViewTest {
     public void testStartProgram() {
         System.out.println("StartProgram");
         StartProgramView instance = new StartProgramView();
-        instance.StartProgram();
+        instance.startProgram();
     }
     
 }
