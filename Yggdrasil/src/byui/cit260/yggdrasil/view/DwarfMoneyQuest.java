673cffeab2e815c22a27932052854f98b217b9dc
Tanvar2 <Tanvar2@gmail.com>
2015-11-03 09:48:53 -0700

    11/3/2015 Created and commented DwarfMoneyQuest

---
+++ Yggdrasil/src/byui/cit260/yggdrasil/view/DwarfMoneyQuest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.yggdrasil.view;
+
+/**
+ *
+ * @author Brock
+ */
+public class DwarfMoneyQuest {
+    //give player question
+    //accept the player's response
+    //check player answer
+    //tell player if they are correct
+    //if they are correct give them a potion
+    
+}
