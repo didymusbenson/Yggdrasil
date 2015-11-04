61b795736089bd7cb2694b9e2d844880c2ef6005
Didymus <Didymus.Benson@gmail.com>
2015-11-04 09:46:00 -0700

    10-30-2015 more work on combat -ADB

--- Yggdrasil/src/byui/cit260/yggdrasil/control/CombatControl.java
+++ Yggdrasil/src/byui/cit260/yggdrasil/control/CombatControl.java
@@ -4,37 +4,173 @@
  * Description: This is the control class that deals with combat situations.
  */
 package byui.cit260.yggdrasil.control;
+
 import byui.cit260.yggdrasil.model.Actor;
 import byui.cit260.yggdrasil.model.Enemy;
+import byui.cit260.yggdrasil.model.Item;
 import byui.cit260.yggdrasil.model.MainCharacter;
 import java.io.Serializable;
 import java.util.Random;
+
 /**
  *
- * @author Didymus Benson 
+ * @author Didymus Benson
  */
-public class CombatControl implements Serializable{
+public class CombatControl implements Serializable {
 
+    //PLACEHOLDER METHOD, creates a test BOB enemy.
     public static Enemy getEnemy() {
         Enemy bob = new Enemy();
         bob.setEnemyName("BOB");
         bob.setActorMaxHp(30);
-        bob.setActorTempHp(bob.getActorTempHp());
+        bob.setActorTempHp(bob.getActorMaxHp());
+        bob.setActorAttack(5);
+        bob.setActorDefense(5);
+        bob.setEnemyEscapeChance(10);
+        bob.setEnemyGoldReward(50);
+        bob.setEnemyXpReward(50);
         return bob;
     }
-    
-    public Boolean compareRolls(int roll1, int roll2){
 
-        if (roll1 > roll2)
+    //LIKE getEnemy() this is a placeholder for the ACTUAL getHero function
+    public static MainCharacter getHero() {
+        Item tempweap = new Item();
+        tempweap.setItemModifier(5);
+        
+        MainCharacter hero = new MainCharacter();
+        
+        hero.setActorAttack(5);
+        hero.setActorDefense(5);
+        hero.setActorMaxHp(100);
+        hero.setActorTempHp(hero.getActorMaxHp());
+        hero.setMainCharacterCurrentLevel(1);
+        hero.setMainCharacterLuck(5);
+        hero.setMainCharacterMoney(0);
+        hero.setMainCharacterSpeed(5);
+        hero.setMainCharacterTotalXp(0);
+        hero.setActorWeapon(tempweap);
+   
+        return hero;
+    }
+
+    /*
+        INCREASESTAT AND DECREASE STAT may be subject to change
+        but for now they serve the purpose they are suppoed to. 
+        It's up to the programmer to remember the switch cases.
+    */
+    public static void increaseStat(MainCharacter hero, String statName, int amount) {
+        statName = statName.toUpperCase();
+        int temp;
+        switch (statName) {
+            case "XP":
+                temp = hero.getMainCharacterTotalXp();
+                temp += amount;
+                hero.setMainCharacterTotalXp(temp);
+                break;
+            case "HP":
+                temp = hero.getActorMaxHp();
+                temp += amount;
+                hero.setActorMaxHp(temp);
+                break;
+            case "ATK":
+                temp = hero.getActorAttack();
+                temp += amount;
+                hero.setActorAttack(temp);
+                break;
+            case "DEF":
+                temp = hero.getActorDefense();
+                temp += amount;
+                hero.setActorDefense(temp);
+                break;
+            case "LUCK":
+                temp = hero.getMainCharacterLuck();
+                temp += amount;
+                hero.setMainCharacterLuck(temp);
+                break;
+            case "SPD":
+                temp = hero.getMainCharacterSpeed();
+                temp += amount;
+                hero.setMainCharacterSpeed(temp);
+                break;
+            case "GOLD":
+                temp = hero.getMainCharacterMoney();
+                temp += amount;
+                hero.setMainCharacterMoney(temp);
+                break;
+            case "LVL":
+                temp = hero.getMainCharacterCurrentLevel();
+                temp += amount;
+                hero.setMainCharacterCurrentLevel(temp);
+                break;
+            default:
+                System.out.println("INTERNAL ERROR! SOMETHING WENT VERY WRONG!");
+                break;
+        }
+    }
+
+    //WORK IN PROGRESS
+    public static void decreaseStat(MainCharacter hero, String statName, int amount) {
+        statName = statName.toUpperCase();
+        int temp;
+        switch (statName) {
+            case "XP":
+                temp = hero.getMainCharacterTotalXp();
+                temp -= amount;
+                hero.setMainCharacterTotalXp(temp);
+                break;
+            case "HP":
+                temp = hero.getActorMaxHp();
+                temp -= amount;
+                hero.setActorMaxHp(temp);
+                break;
+            case "ATK":
+                temp = hero.getActorAttack();
+                temp -= amount;
+                hero.setActorAttack(temp);
+                break;
+            case "DEF":
+                temp = hero.getActorDefense();
+                temp -= amount;
+                hero.setActorDefense(temp);
+                break;
+            case "LUCK":
+                temp = hero.getMainCharacterLuck();
+                temp -= amount;
+                hero.setMainCharacterLuck(temp);
+                break;
+            case "SPD":
+                temp = hero.getMainCharacterSpeed();
+                temp -= amount;
+                hero.setMainCharacterSpeed(temp);
+                break;
+            case "GOLD":
+                temp = hero.getMainCharacterMoney();
+                temp -= amount;
+                hero.setMainCharacterMoney(temp);
+                break;
+            case "LVL":
+                temp = hero.getMainCharacterCurrentLevel();
+                temp -= amount;
+                hero.setMainCharacterCurrentLevel(temp);
+                break;
+            default:
+                System.out.println("INTERNAL ERROR! SOMETHING WENT VERY WRONG!");
+                break;
+        }
+    }
+
+    public Boolean compareRolls(int roll1, int roll2) {
+
+        if (roll1 > roll2) {
             return true;
-        else if (roll2 < roll1)
+        } else if (roll2 < roll1) {
             return false;
-        else {
+        } else {
             return tieBreaker();
         }
     }
-    
-    public int rollRandRange(int low, int high){
+
+    public int rollRandRange(int low, int high) {
         int roll;
         Random r = new Random();
         int n = high - low + 1;  // N = difference of high range and low range
@@ -42,50 +178,46 @@ public class CombatControl implements Serializable{
         roll = low + i;          // roll = low number + random number
         return roll;
     }
-    
-    public Boolean tieBreaker(){
+
+    public Boolean tieBreaker() {
         Random r = new Random();
         int i = r.nextInt(2);
         return i == 1; // If i is 1, it returns true, if not returns false.
     }
-    
-    public Boolean runAway(MainCharacter hero, Enemy enemy){
+
+    public Boolean runAway(MainCharacter hero, Enemy enemy) {
         // The player's minimum chance of running away is the player's base speed
         int runRoll = rollRandRange(hero.getMainCharacterSpeed(), 100);
         return compareRolls(runRoll, enemy.getEnemyEscapeChance());
     }
-    
+
     /* Player attack and enemy attack could be merged by passing ACTOR as a 
-    parameter instead of MainCharacter and Enemy. The issue is that we have
-    to pass the MainCharacter or enemey classes later to the calc damage 
-    functions.-ADB
-    */
-    public Boolean tryAttack(Actor attacker, Actor defender){
+     parameter instead of MainCharacter and Enemy. The issue is that we have
+     to pass the MainCharacter or enemey classes later to the calc damage 
+     functions.-ADB
+     */
+    public Boolean tryAttack(Actor attacker, Actor defender) {
         System.out.println("CombatControl.playerAttack() called.");
-        int attackRoll = rollRandRange(1, 20) + attacker.getActorAttack(); 
-        if (attackRoll > defender.getActorDefense())
-            return true;
-        else
-            return false;
+        int attackRoll = rollRandRange(1, 20) + attacker.getActorAttack();
+        return attackRoll > defender.getActorDefense();
     }
 
-    
-    public void defend(Actor actor){
-        System.out.println("CombatControl.defend() called.");        
+    public void defend(Actor actor) {
+        System.out.println("CombatControl.defend() called.");
         // TODO MAKE FUNCTION.
     }
 
-    private int calcPlayerDamage(MainCharacter hero) {
+    public int calcPlayerDamage(MainCharacter hero) {
         System.out.println("calcDamage() called");
         // TODO, WRITE FUNCTION
-        int damage = hero.getActorAttack() + 
-                rollRandRange(1, hero.getActorWeapon().getItemModifier());
+        int damage = hero.getActorAttack()
+                + rollRandRange(1, hero.getActorWeapon().getItemModifier());
         return damage;
     }
 
-    private void applyDamage(int damage, Actor defender) {
+    public void applyDamage(int damage, Actor defender) {
         System.out.println("applyDamage() called");
-        // TODO, MAKE FUNCTION
+        int newHp = defender.getActorTempHp() - damage;
+        defender.setActorTempHp(newHp);
     }
 }
-    
--- Yggdrasil/src/byui/cit260/yggdrasil/view/CombatMenuView.java
+++ Yggdrasil/src/byui/cit260/yggdrasil/view/CombatMenuView.java
@@ -16,31 +16,45 @@ import java.util.Scanner;
  * @author tsunami
  */
 public class CombatMenuView implements Serializable {
+
     private final String BANNER = "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
     private final String NORMAL = "You are attacked by a "; // add monster name
     private final String SNEAK = "You snuck up on a "; // add monster name
     private final String AMBUSH = "A monster gets the jump on you!";
     private final String VICTORY = "You have defeated the "; //add monster name
     private final String OPTIONS = "(R)un - (A)ttack - (I)tem - (D)defend";
-    
+
     public void displayMenu() {
         Enemy enemy = CombatControl.getEnemy();
+        MainCharacter hero = CombatControl.getHero(); //SOMETHING needs to pull the hero from SOMEWHERE
         System.out.println(BANNER);
-        System.out.println(NORMAL + enemy.getEnemyName());
+        System.out.println(NORMAL + enemy.getEnemyName() + "!");
         char selection = ' ';
-        
+
         do {
+            int round = 0; // rounds of combat, increases as the battle goes on.
+            if (round > 0);
+            {
+                System.out.println("Your HP: " + hero.getActorTempHp()
+                        + "\n" + enemy.getEnemyName() + "'s HP: " + enemy.getActorTempHp());
+            }
             System.out.println(OPTIONS);
             String input = this.getInput(); // get selection
             input = input.toUpperCase();
             selection = input.charAt(0); // grab first letter, no matter what's typed
-            this.doAction(selection, null /*main character*/, enemy);
-            // ADD Player and Monster temp HPs here.
+            this.doAction(selection, hero, enemy);
+
+            round++;
         } while (enemy.getActorTempHp() > 0); // or until player dies.
-        
+
         System.out.println(VICTORY + enemy.getEnemyName() + "!");
+        System.out.println("You were awarded " + enemy.getEnemyXpReward() + " XP!");
+        CombatControl.increaseStat(hero, "XP", enemy.getEnemyXpReward());
+        System.out.println("You have found " + enemy.getEnemyGoldReward() + " gold!");
+        CombatControl.increaseStat(hero, "GOLD", enemy.getEnemyGoldReward());
+        System.out.println(BANNER);
     }
-    
+
     public String getInput() {
         String input = null;
         Boolean valid = false;
@@ -59,27 +73,22 @@ public class CombatMenuView implements Serializable {
 
         return input;
     }
+
     public void doAction(char selection, MainCharacter hero, Enemy enemy) {
 
         switch (selection) {
             case 'R':
                 System.out.println("RUN CHOSEN");
-                //CombatControl.runAway(hero, enemy);               
+                CombatControl.runAway(hero, enemy);               
                 break;
             case 'A':
-                System.out.println("ATTACK CHOSEN");
-                /* if (!CombatControl.tryAttack())
-                       System.out.println("Your attack missed!";
-                else{
-                       int damage = calcPlayerDamage(MainCharacter);
-                       applyDamage(damage, enemy);
+                if (!CombatControl.tryAttack(hero, enemy)) {
+                    System.out.println("Your attack missed!";
+                } else {
+                    int damage = CombatControl.calcPlayerDamage(hero);
+                    System.out.println("You hit the " + enemy.getEnemyName() + " for " + damage + " damge!");
+                    CombatControl.applyDamage(damage, enemy);
                 }
-                // Must call a function that rolls successful attack 
-                // and then roll damage of the attack
-                // CombatControl.playerAttack(MainCharacter, Enemy);
-                // ** attack() then calls rollSuccess() and applyDamage() ** 
-                        
-                        */
                 break;
             case 'I':
                 System.out.println("ITEM CHOSEN");
