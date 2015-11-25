28b50d731597d19555c1c5b3f5e35db38605ad8f
Tanvar2 <Tanvar2@gmail.com>
2015-11-25 14:12:14 -0700

    11/25/2015 15:05 Began making exceptions for the CombatControl.java class.

---
+++ Yggdrasil/src/Exceptions/CombatControlException.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exceptions;
+
+/**
+ *
+ * @author Brock
+ */
+public class CombatControlException extends Exception{
+
+    public CombatControlException() {
+    }
+
+    public CombatControlException(String message) {
+        super(message);
+    }
+
+    public CombatControlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CombatControlException(Throwable cause) {
+        super(cause);
+    }
+
+    public CombatControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    
+}
