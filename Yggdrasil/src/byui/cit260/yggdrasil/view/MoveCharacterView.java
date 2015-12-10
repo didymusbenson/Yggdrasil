5838269c21da27c2e39e5630f686f97e7292a941
Tanvar2 <Tanvar2@gmail.com>
2015-12-10 10:13:03 -0700

    12/10/2015 -edited new class to be able to move actor to new coordinates.

--- Yggdrasil/src/byui/cit260/yggdrasil/view/MoveCharacterView.java
+++ Yggdrasil/src/byui/cit260/yggdrasil/view/MoveCharacterView.java
@@ -5,13 +5,32 @@
 package byui.cit260.yggdrasil.view;
 
 import byui.cit260.yggdrasil.view.GameMenuView;
+import yggdrasil.Yggdrasil;
 
 /**
  *
  * @author Brock
  */
-public class MoveCharacterView {
+public class MoveCharacterView extends View {
+
+    public MoveCharacterView() {
+        super("Which direction do you wish to travel? N - Notrh, S - South, "
+                + "E - East, W - West");
+    }
+
+    @Override
+    public boolean doAction(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     //Ask user for N,S,E,W movement
+    private static class MoveChoice {
+
+        public MoveChoice() {
+        }
+    }
+
+}
+        
     //Check to make sure the input is a valid direction
     //Check to make sure that you have a valid actor
     
@@ -20,5 +39,3 @@ public class MoveCharacterView {
     //add or subtract from row or column depending on what direction is given
     //assign new coordinates to the Point coordinates 
     //give message confirming that the coordiantes have been changed.
-
-}
