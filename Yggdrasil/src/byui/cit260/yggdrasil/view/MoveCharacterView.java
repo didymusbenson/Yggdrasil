d9f35453fe68040c8d816930bcf1851510aaa647
Tanvar2 <Tanvar2@gmail.com>
2015-12-10 09:44:00 -0700

    12/10/2015 - created new class to be able to move actor to new coordinates.

---
+++ Yggdrasil/src/byui/cit260/yggdrasil/view/MoveCharacterView.java
@@ -0,0 +1,24 @@
+/*
+ * This class moves the actor from one location to another as directed by the 
+ * user. This class is called by the GameMenuView class.
+ */
+package byui.cit260.yggdrasil.view;
+
+import byui.cit260.yggdrasil.view.GameMenuView;
+
+/**
+ *
+ * @author Brock
+ */
+public class MoveCharacterView {
+    //Ask user for N,S,E,W movement
+    //Check to make sure the input is a valid direction
+    //Check to make sure that you have a valid actor
+    
+    //If everything is valid call the actor class from the model layer
+    //get the Point coordinates from the actor class
+    //add or subtract from row or column depending on what direction is given
+    //assign new coordinates to the Point coordinates 
+    //give message confirming that the coordiantes have been changed.
+
+}
